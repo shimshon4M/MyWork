e7df0836b63027ebdd38170b565fd1dc8482ea50
momoi <ma17118@shibaura-it.ac.jp>
2017-08-26 13:47:38 +0900

    add

--- texCompiler.py
+++ texCompiler.py
@@ -1,4 +1,4 @@
-#!usr/bin/env python
+#coding:utf-8
 
 import xml.etree.ElementTree as ET
 import xml.dom.minidom as minidom
@@ -16,7 +16,7 @@ CURLIES_COMMANDS=[
     ,"received"
     ,"revised"
     ,"accepted"
-    ,"setcounter"
+    #,"setcounter"
     ,"jtitle"     #日本語題目
     ,"jauthor"    #日本語著者
     ,"jabstract"  #日本語概要
@@ -32,7 +32,7 @@ CURLIES_COMMANDS=[
     ,"input"      #外部からのデータ入力 表を作るときなどに使用
     ,"addtolength"#表の設定か何か？
     ,"notice"
-    ,"bibliographystyle" #
+    #,"bibliographystyle" #
     ,"bioauthor"    #著者情報
     ,"bibitem"      #参考文献１件に関するブロック
 ] #command{}*
@@ -41,10 +41,24 @@ REF_COMMANDS=[
     "affiref"     #所属参照 「†」
     ,"affilabel"  #所属被参照 「†○○大学」
     ,"footnote"   #フッタ参照 「*」
+    ,"footnotetext" #〃
     ,"ref"        #論文内セクション参照 「*節」
     ,"cite"       #引用 「[*]」
     ,"label"      #参照じゃないけど読み飛ばすからとりあえず
-]#参照、引用
+    ,"bibliographystyle"
+    ,"bibliography"
+    ,"biotitle"
+    ,"bioreceived"
+    ,"biorevised"
+    ,"bioaccepted"
+    ,"caption"
+    ,"setcounter"
+    ,"hspace"
+]#参照、引用ほか 直後にくるカッコ内は無視する
+TREE_COMMANDS=[
+    "tree" #command[]{}
+    ,"leaf"#command{}
+]#ツリー 中身は無視する
 SECTION_COMMANDS=[
     "section"           # 大節
     ,"subsection"       # 小節
@@ -71,7 +85,11 @@ DECORATE_COMMANDS=[
     ,"Bbf"
 ]# 文字の装飾 後ろには文字だけまたは{}？
 nonCommands=[
-    "maketitle","Jetal","protect","biodate"
+    "maketitle"
+    ,"Jetal"
+    ,"protect"
+    ,"biodate"
+    ,"footmark"
 ]# 何も装飾せずコマンドだけのもの
 SYMBOL_COMMANDS=[
     "BBOP"  # (
@@ -96,26 +114,25 @@ ITEM_TYPE=[
 def main(filename):
     text=readFile(filename)
     xml_tree=analyze(text)
-    writeFile(xml_tree)
+    writeFile(filename,xml_tree)
 
 def readFile(filename):
     with open(filename,"r")as f:
         text=f.read()
     return text.replace("\n","")
 
-def writeFile(xml_tree):
+def writeFile(in_filename,xml_tree):
     pretty_string=minidom.parseString(ET.tostring(xml_tree,"utf-8")).toprettyxml(indent="  ")
-    with open("text.xml","w")as f:
+    with open(in_filename+".xml","w")as f:
         f.write(pretty_string)
 
 def analyze(text):
     root=ET.Element("root") #xmlのroot
     i=0
-    while i<len(text):
-    #for i in range(len(text)):
+    while i<len(text):#1文字ずつ読んでいく
         read_text=""
         if text[i]=="\\":
-            cmd,c_len=readCommand(text[i+1:])
+            cmd,c_len=readCommand(text[i+1:])#コマンドとその長さ
             i+=c_len
             #print(cmd)
             if cmd in BOX_CURLY_COMMANDS:
@@ -142,6 +159,8 @@ def analyze(text):
             elif cmd in REF_COMMANDS:
                 txt,t_len=readText(text[i+1:],"CURLY")
                 i+=t_len
+            #elif cmd in BEGIN_END_COMMANDS:
+                
         i+=1
     return root
                 
@@ -168,16 +187,15 @@ def readCommand(text):
 def readText(text,brack_type=None):
     OBRACK={"BOX":"[","CURLY":"{","NOBRACK":""}
     CBRACK={"BOX":"]","CURLY":"}","NOBRACK":""}
-    open_curly_brack_num=0
-    close_curly_brack_num=0
+    open_curly_brack_num=0#開き括弧の数
+    close_curly_brack_num=0#閉じ括弧の数
     isEndSection=False
     isUpBegin=False
     read_text=""
     if brack_type not in ["OBRACK","CBRACL","NOBRACK"] and text[0]!=OBRACK[brack_type]: #指定の括弧で始まらなければ空の読み取り文字列として返す
         return "",0
     i=0
-    while i<len(text):
-    #for i in range(len(text)):
+    while i<len(text):#1文字ずつ読んでいく
         if isUpBegin:
             if text[i]!="\\":
                 i+=1
@@ -196,17 +214,16 @@ def readText(text,brack_type=None):
             inner_read_text,t_len=readText(text[i+1:],"CURLY")
             i+=t_len
             if cmd in REF_COMMANDS:
-                i+=1
+                txt,tmp_t_len=readText(text[i+1:],"CURLY")
+                i+=tmp_t_len
+                #i+=1
                 continue
             elif cmd in SECTION_COMMANDS or cmd=="acknowledgment":
                 i-=t_len
                 i-=c_len
                 isEndSection=True
-            elif cmd in BEGINEND_COMMANDS and inner_read_text=="table":
-                if isUpBegin:
-                    isUpBegin=False
-                else:
-                    isUpBegin=True
+            elif cmd in BEGINEND_COMMANDS and inner_read_text in ["table","center","figure","tabular","screen"]:
+                    isUpBegin=not isUpBegin
             else:
                 read_text+=inner_read_text
         else:
