01f43762626beb53cefe635a5acb7263329f546f
momoi <ma17118@shibaura-it.ac.jp>
2018-01-09 13:20:16 +0900

    add

--- wordExtract/appendLabel.py
+++ wordExtract/appendLabel.py
@@ -2,13 +2,13 @@ import sys
 import os.path
 
 def append_label(filename):
+    #existの方使わないように！
     if os.path.exists(filename.replace("BoW","labeled")):
         with open(filename.replace("BoW","labeled"),"r")as f:
             exist_data=[line for line in f.readlines()]
         outdata=[]
         with open(filename,"r")as f:
-            lines=f.readlines()
-            for i,(line_feature,line_exist) in enumerate(zip(lines,exist_data),1):
+            for i,line_feature in enumerate(lines,1):
                 if i%10==1:
                     print("----0:not term 1:raw data 2:formated data 3:method 4:goal----")
                 line_spl=line_feature.split("\t")
@@ -34,8 +34,8 @@ def append_label(filename):
                 line_spl=line.split("\t")
             
                 #作業サポート用print
-                back_text=[s for s in line.split("\t")[4:8]]
-                fromt_text=[s for s in line.split("\t")[8:12]]
+                back_text=[s for s in line.split("\t")[3:7]]
+                fromt_text=[s for s in line.split("\t")[7:11]]
                 print(str(i)+"/"+str(len(lines)),"".join(back_text)+"<"+line_spl[0]+">"+"".join(fromt_text),":",end="")
                 label=input()
                 outdata.append(line_spl[0]+"\t"+line_spl[1]+"\t"+label+"\n")
